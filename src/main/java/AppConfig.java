ee5e91ab337d6664c080abe5027333609059bf64
GoydykovaKristi <K4154492@yandex.ru>
2021-03-11 23:26:55 +0300

    zadacha 2.1.2 new

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat myCat = new Cat();
+        myCat.setName("Barsik");
+        return myCat;
+    }
 }
